983d5808c365213ed0f744bfc1beaf9b2a198af7
dead-horse <dead_horse@qq.com>
2014-10-22 01:49:32 +0800

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+koa.io
+---------------
+
+[![NPM version][npm-image]][npm-url]
+[![build status][travis-image]][travis-url]
+[![Test coverage][coveralls-image]][coveralls-url]
+[![David deps][david-image]][david-url]
+[![node version][node-image]][node-url]
+[![Gittip][gittip-image]][gittip-url]
+
+[npm-image]: https://img.shields.io/npm/v/koa.io.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/koa.io
+[travis-image]: https://img.shields.io/travis/koajs/koa.io.svg?style=flat-square
+[travis-url]: https://travis-ci.org/koajs/koa.io
+[coveralls-image]: https://img.shields.io/coveralls/koajs/koa.io.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/koajs/koa.io?branch=master
+[david-image]: https://img.shields.io/david/koajs/koa.io.svg?style=flat-square
+[david-url]: https://david-dm.org/koajs/koa.io
+[node-image]: https://img.shields.io/badge/node.js-%3E=_0.10-green.svg?style=flat-square
+[node-url]: http://nodejs.org/download/
+[gittip-image]: https://img.shields.io/gittip/dead-horse.svg?style=flat-square
+[gittip-url]: https://www.gittip.com/dead-horse/
+
+realtime web framework combine koa and socket.io
+
+## Installation
+
+```bash
+$ npm install koa.io
+```
+
+## Usage
+
+### License
+
+MIT
---
+++ example/chat/index.js
@@ -0,0 +1,86 @@
+var staticCache = require('koa-static-cache');
+var koa = require('../..');
+var path = require('path');
+var fs = require('fs');
+
+var app = koa();
+
+var port = process.env.PORT || 3000;
+
+// Routing
+app.use(staticCache(path.join(__dirname, 'public')));
+
+app.use(function*() {
+  this.body = fs.createReadStream(path.join(__dirname, 'public/index.html'));
+  this.type = 'html';
+});
+
+app.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Chatroom
+
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
+
+app.io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    if (addedUser) {
+      delete usernames[socket.username];
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
