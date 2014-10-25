3d7fed06b09ecd2e1f95397ee6ab92c10bac1ec8
dead-horse <dead_horse@qq.com>
2014-10-25 14:14:24 +0800

    delegate socket to context
    make `use` and `route`'s context the same

--- example/chat/index.js
+++ example/chat/index.js
@@ -46,47 +46,45 @@ app.io.use(function* userLeft(next) {
  * router for socket event
  */
 
-// when the client emits 'new message', this listens and executes
-app.io.route('new message', function* () {
-  // we tell the client to execute 'new message'
-  this.socket.broadcast.emit('new message', {
-    username: this.socket.username,
-    message: this.args[0]
-  });
-});
-
 app.io.route('add user', function* () {
   // we store the username in the socket session for this client
-  var username = this.args[0];
-  var socket = this.socket;
-
-  socket.username = username;
+  var username = this.username = this.args[0];
   // add the client's username to the global list
   usernames[username] = username;
   ++numUsers;
-  socket.addedUser = true;
-  socket.emit('login', {
+  this.addedUser = true;
+  this.emit('login', {
     numUsers: numUsers
   });
+
   // echo globally (all clients) that a person has connected
-  socket.broadcast.emit('user joined', {
-    username: socket.username,
+  this.broadcast.emit('user joined', {
+    username: this.username,
     numUsers: numUsers
+  });
+});
+
+// when the client emits 'new message', this listens and executes
+app.io.route('new message', function* () {
+  // we tell the client to execute 'new message'
+  this.broadcast.emit('new message', {
+    username: this.username,
+    message: this.args[0]
   });
 });
 
 // when the client emits 'typing', we broadcast it to others
 app.io.route('typing', function* () {
-  console.log('%s is typing', this.socket.username);
-  this.socket.broadcast.emit('typing', {
-    username: this.socket.username
+  console.log('%s is typing', this.username);
+  this.broadcast.emit('typing', {
+    username: this.username
   });
 });
 
 // when the client emits 'stop typing', we broadcast it to others
 app.io.route('stop typing', function* () {
-  console.log('%s is stop typing', this.socket.username);
-  this.socket.broadcast.emit('stop typing', {
-    username: this.socket.username
+  console.log('%s is stop typing', this.username);
+  this.broadcast.emit('stop typing', {
+    username: this.username
   });
 });
